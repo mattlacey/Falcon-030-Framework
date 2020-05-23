76c0780b7705b8a01545cde5309810c7ece58d02
Matt Lacey <lacey@proximityinsight.com>
2020-05-24 00:04:36 +1000

    Adds triangle edge buffers to avoid issues caused by calculating two lines at once where the y coordinates would get out of sync on each side based on the gradients

--- TRI.C
+++ TRI.C
@@ -20,13 +20,9 @@ void renderTri(Tri *pTri, void *pBuffer)
 	V3 *top = &verts[0];
 	V3 *mid = &verts[1];
 	V3 *bot = &verts[2];
-	V3 *left;
-	V3 *right;
 	V3 *temp;
-	V3 *target;
 
-	long x[2], y[2], dx[2], dy[2], sx[2], sy[2], err[2], err2[2];
-	long safe = 1000;
+	long bounds[2][240];
 	int i;
 
 	if(mid->y < top->y)
@@ -50,114 +46,65 @@ void renderTri(Tri *pTri, void *pBuffer)
 		mid = temp;
 	}
 	
-	x[0] = x[1] = top->x;
-	y[0] = y[1] = top->y;
-	
 	if(mid->x <= bot->x)
 	{
-		left = mid;
-		right = bot;
+		calcSpanBounds(bounds[0], top->x, top->y, mid->x, mid->y); 
+		calcSpanBounds(bounds[0], mid->x, mid->y, bot->x, bot->y); 
+		calcSpanBounds(bounds[1], top->x, top->y, bot->x, bot->y); 
 	}
 	else
 	{
-		left = bot;
-		right = mid;
+		calcSpanBounds(bounds[1], top->x, top->y, mid->x, mid->y); 
+		calcSpanBounds(bounds[1], mid->x, mid->y, bot->x, bot->y); 
+		calcSpanBounds(bounds[0], top->x, top->y, bot->x, bot->y); 
 	}
 
-/*	
-	renderLine(top->x, top->y, left->x, left->y, 0x8f00, pBuffer);
-	renderLine(top->x, top->y, right->x, right->y, 0x00ff, pBuffer);
-*/
-
-	dx[0] = (top->x < left->x ? left->x - top->x : top->x - left->x);
-	dx[1] = (top->x < right->x ? right->x - top->x : top->x - right->x);
-
-	sx[0] = (top->x < left->x ? 1 : -1);
-	sx[1] = (top->x < right->x ? 1 : -1);
-
-/*	long dy = - (y1 < y2 ? y2 - y1 : y1 - y2);
-	long sy = (y1 < y2 ? 1 : -1);
-*/	
-	dy[0] = - (left->y - top->y);
-	dy[1] = - (right->y - top->y);
-
-	sy[0] = 1;
-	sy[1] = 1;
-	
-	err[0] = dx[0] + dy[0];
-	err[1] = dx[1] + dy[1];
-	
-	if(top->y == mid->y)
-	{
-		x[(left == mid ? 0 : 1)] = mid->x;
-		y[(left == mid ? 0 : 1)] = mid->y;
-	}
-	
-	target = mid;
-
-	while(safe)
-	{
-		renderSpan(x[0], x[1], y[0], pTri->col, pBuffer);
-		/*
-		*((unsigned int*)pBuffer + x[0] + (y[0] * (long)320)) = col;
-		*((unsigned int*)pBuffer + x[1] + (y[1] * (long)320)) = col;
-		*/
-
-		if((x[0] == target->x && y[0] == target->y)
-			|| (x[1] == target->x && y[1] == target->y))
-		{
-			if(target == bot)
-			{
-				break;
-			}
-			else
-			{
-				/* First half done - recalculate left/right accordingly */
-				target = bot;
-				
-				i = (x[0] == mid->x && y[0] == mid->y) ? 0 : 1;
-				
-				dx[i] = (mid->x < bot->x ? bot->x - mid->x : mid->x - bot->x);
-				dy[i] = - (bot->y - mid->y);
-				sx[i] = (mid->x < bot->x ? 1 : -1);
-				dy[i] = - (bot->y - mid->y);
-				err[i] = dx[i] + dy[i];
-			}
-		}
-
-		err2[0] = 2 * err[0];
-		err2[1] = 2 * err[1];
-		
-		if(err2[0] >= dy[0])
-		{
-			err[0] += dy[0];
-			x[0] += sx[0];
-		}
-		
-		if(err2[1] >= dy[1])
-		{
-			err[1] += dy[1];
-			x[1] += sx[1];
-		}
-		
-		if(err2[0] <= dx[0])
-		{
-			err[0] += dx[0];
-			y[0] += sy[0];
-		}
-		
-		if(err2[1] <= dx[1])
-		{
-			err[1] += dx[1];
-			y[1] += sy[1];
-		}
-		
-		safe--;
-	}
-		
 	renderLine(top->x, top->y, bot->x, bot->y, 0xffe0, pBuffer);
 	renderLine(top->x, top->y, mid->x, mid->y, 0x0cff, pBuffer);
 	renderLine(mid->x, mid->y, bot->x, bot->y, 0xf81f, pBuffer); 
+
+	for(i = top->y; i <= bot->y; i++)
+	{
+		renderSpan(bounds[0][i], bounds[1][i], i, pTri->col, pBuffer);
+	}
+}
+
+void calcSpanBounds(long *boundBuffer, long x1, long y1, long x2, long y2)
+{
+	long x = x1;
+	long y = y1;
+	long dx = (x1 < x2 ? x2 - x1 : x1 - x2);
+	long sx = (x1 < x2 ? 1 : -1);
+
+	long dy = - (y1 < y2 ? y2 - y1 : y1 - y2);
+	long sy = (y1 < y2 ? 1 : -1);
+	long ymod = 0;
+	long err = dx + dy;
+	long err2;
+
+	while(1)
+	{
+		if(x == x2 && y == y2)
+		{
+			boundBuffer[y + ymod] = x;
+			break;
+		}
+		
+		err2 = 2 * err;
+		
+		if(err2 >= dy)
+		{
+			err += dy;
+			x += sx;
+		}
+		
+		if(err2 <= dx)
+		{
+			boundBuffer[y + ymod] = x;
+			err += dx;
+			y += sy;
+		}
+	}
 }
 
 void renderSpan(long x1, long x2, long y, unsigned col, void *pBuffer)
